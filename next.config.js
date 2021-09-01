6ba54f150dd68a4f85adfcd6b93f2a42686170fe
Oded Winberger <odedindi@gmail.com>
2021-09-02 01:13:32 +0200

    added PWA abilities, remove server, start to create color schemes for 'bright/dark options

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
 const { i18n } = require('./next-i18next.config');
 
-module.exports = {
+module.exports = withPWA({
+	pwa: {
+		dest: 'public',
+		disable: process.env.NODE_ENV === 'development',
+	},
 	reactStrictMode: true,
 	i18n,
 	react: {
 		useSuspense: true,
 	},
-};
+});
