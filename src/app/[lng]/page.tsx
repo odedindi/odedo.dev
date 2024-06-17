3733d376853e3b761573af2db72becbed94f0f10
odi <odi@panter.ch>
2024-06-17 09:33:19 +0200

    Refactor page.tsx to use projects.json

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,9 +1,11 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
+import { Fragment } from "react";
 import { useTranslation } from "../i18n";
-import { Language } from "next/router";
+import type { Language } from "next/router";
 import { Space, Text, Title } from "@mantine/core";
 import MyImage from "../components/myImage";
 import Project from "../components/Project";
+import projects from "./projects.json";
 
 const Page: NextPage<{ params: { lng: Language } }> = async ({
 	params: { lng },
@@ -17,42 +19,19 @@ const Page: NextPage<{ params: { lng: Language } }> = async ({
 			<Title>{t("title")} 👋</Title>
 			<Space h="xl" />
 			<Text>{t("preface")}</Text>
-			<Space h="xl" />
-			<Project
-				title={t("projects.landpro.title")}
-				description={t("projects.landpro.description")}
-				imageSrc="https://land-pro.vercel.app/favicon.png"
-				link="https://land-pro.vercel.app/"
-				tecStack={t("projects.landpro.tecStack")}
-				repo="https://github.com/odedindi/landpro_landing"
-			/>
-			<Space h="xl" />
-			<Project
-				title={t("projects.geoChat.title")}
-				description={t("projects.geoChat.description")}
-				imageSrc="https://geo-chat-omega.vercel.app/assets/beacon.png"
-				link="https://geo-chat-omega.vercel.app/home"
-				tecStack={t("projects.geoChat.tecStack")}
-				repo="https://github.com/odedindi/GeoChat"
-			/>
-			<Space h="xl" />
-			<Project
-				title={t("projects.solr.title")}
-				description={t("projects.solr.description")}
-				imageSrc="https://solr.vercel.app/icons/apple-touch-icon.png"
-				link="https://solr.vercel.app/"
-				tecStack={t("projects.solr.tecStack")}
-				repo="https://github.com/odedindi/solr"
-			/>
-			<Space h="xl" />
-			<Project
-				title={t("projects.dialektli.title")}
-				description={t("projects.dialektli.description")}
-				imageSrc="https://www.dialektli.ch/assets/dialektli_logo.svg"
-				link="https://www.dialektli.ch/"
-				tecStack={t("projects.dialektli.tecStack")}
-				repo="https://github.com/urbanswisstionary/dialektli/"
-			/>
+			{projects.map((p, i) => (
+				<Fragment key={i}>
+					<Space h="xl" />
+					<Project
+						title={t(`projects.${p.title}`)}
+						description={t(`projects.${p.description}`)}
+						imageSrc={p.imageSrc}
+						link={p.link}
+						tecStack={t(`projects.${p.tecStack}`)}
+						repo={p.repo}
+					/>
+				</Fragment>
+			))}
 		</>
 	);
 };
