e3288cd190db65ad99a860c1b137005888491a95
odi <odedindi@gmail.com>
2025-04-06 20:54:52 +0200

    Remove link from Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { useLocale } from "next-intl";
-import Link from "next/link";
-
 import type React from "react";
 
 import { LanguageSwitcher } from "./language-switcher";
 import { DesktopMenu, MobileMenu } from "./nav-bar";
 
 export function Header() {
-	const locale = useLocale();
-
 	return (
 		<header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
 			<div className="w-full flex h-16 items-center justify-between px-4">
@@ -18,12 +13,6 @@ export function Header() {
 				<DesktopMenu />
 				<div className="flex items-center gap-2">
 					<LanguageSwitcher />
-					<Link
-						href={`/${locale}`}
-						className="text-xl font-bold tracking-tight"
-					>
-						ODI
-					</Link>
 				</div>
 			</div>
 		</header>
